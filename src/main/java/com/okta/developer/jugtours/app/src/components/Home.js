390b0b7845e8251f2750e99830c1d55eafc1dd4d
Colin Fendrick <colin.fendrick@gmail.com>
2020-10-21 09:17:44 -0400

    Make the cat a link

--- src/main/java/com/okta/developer/jugtours/app/src/components/Home.js
+++ src/main/java/com/okta/developer/jugtours/app/src/components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Container } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Home = () => (
 	<Container fluid>
 		<div className='card-container'>
-			<div className='card'></div>
+			<div className='card-column'>
+				<h4>WELCOME MY GOOD BITCH</h4>
+				<Link to='/groups'>
+					<div className='card'></div>
+				</Link>
+			</div>
 		</div>
 	</Container>
 );
